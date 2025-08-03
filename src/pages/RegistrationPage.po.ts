00258261e7cbd8dd02256409d1abc6ee43b0a626
bakhmeteva <yuliia.b@taxer.ua>
2025-08-03 18:21:25 +0300

    test.steps

--- src/pages/RegistrationPage.po.ts
+++ src/pages/RegistrationPage.po.ts
@@ -1,5 +1,6 @@
 import { Page, Locator, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
+import { test } from '../fixtures/fixtures';
 
 export class RegistrationPage {
   page: Page;
@@ -7,7 +8,7 @@ export class RegistrationPage {
   passwordInput: Locator;
   nameInput: Locator;
   surnameInput: Locator;
-  privacyPolicy: Locator
+  privacyPolicy: Locator;
   createAccountButton: Locator;
   error: Locator;
 
@@ -17,13 +18,15 @@ export class RegistrationPage {
     this.passwordInput = page.locator('input[data-qa-input-qualifier="password"]');
     this.nameInput = page.locator('input[data-qa-input-qualifier="firstName"]');
     this.surnameInput = page.locator('input[data-qa-input-qualifier="lastName"]');
-    this.privacyPolicy =page.locator(`xpath=.//span[contains(text(), 'I have read and understand')]/ancestor::div[@class='form-input']//span[contains(@class,'form-input-checkbox__label')]`)
+    this.privacyPolicy = page.locator(`xpath=.//span[contains(text(), 'I have read and understand')]/ancestor::div[@class='form-input']//span[contains(@class,'form-input-checkbox__label')]`);
     this.createAccountButton = page.locator('[data-qa-action="sign-up-submit"]');
     this.error = page.locator('.form-input-error');
   }
 
   async clickRegisterButton(): Promise<void> {
-    await this.page.locator('[data-qa-id="logon-view-alternate-button"]').click();
+    await test.step(`clickRegisterButton`, async () => {
+      await this.page.locator('[data-qa-id="logon-view-alternate-button"]').click();
+    });
   }
 
   async clickCreateAccountButton() {
@@ -31,56 +34,64 @@ export class RegistrationPage {
   }
 
 
-
   async registerWithFakeData() {
-    await expect(this.nameInput).toBeVisible({ timeout: 10000 });
-    const name = faker.person.firstName();
-    const surname = faker.person.lastName();
-    const email = faker.internet.email();
-    const password = faker.internet.password();
+    await test.step(`registration`, async () => {
+      await expect(this.nameInput).toBeVisible({ timeout: 10000 });
+      const name = faker.person.firstName();
+      const surname = faker.person.lastName();
+      const email = faker.internet.email();
+      const password = faker.internet.password();
 
-    await this.emailInput.fill(email);
-    await this.passwordInput.fill(password);
-    await this.nameInput.fill(name);
-    await this.surnameInput.fill(surname);
-    await this.surnameInput.press('Tab');
-    await this.privacyPolicy.click();
-    await this.createAccountButton.click();
+      await this.emailInput.fill(email);
+      await this.passwordInput.fill(password);
+      await this.nameInput.fill(name);
+      await this.surnameInput.fill(surname);
+      await this.surnameInput.press('Tab');
+      await this.privacyPolicy.click();
+      await this.createAccountButton.click();
 
-    return { name, surname, email, password };
+      return { name, surname, email, password };
+    });
   }
 
   async registerWithInvalidData() {
-    await expect(this.nameInput).toBeVisible({ timeout: 10000 });
-    const invalidName = '';
-    const invalidSurname = '';
-    const invalidEmail = 'invalid-email';
-    const invalidPassword = '123';
+    await test.step(`registerWithInvalidData`, async () => {
+      await expect(this.nameInput).toBeVisible({ timeout: 10000 });
+      const invalidName = '';
+      const invalidSurname = '';
+      const invalidEmail = 'invalid-email';
+      const invalidPassword = '123';
 
-    await this.nameInput.fill(invalidName);
-    await this.surnameInput.fill(invalidSurname);
-    await this.emailInput.fill(invalidEmail);
-    await this.passwordInput.fill(invalidPassword);
-    await this.createAccountButton.click();
+      await this.nameInput.fill(invalidName);
+      await this.surnameInput.fill(invalidSurname);
+      await this.emailInput.fill(invalidEmail);
+      await this.passwordInput.fill(invalidPassword);
+      await this.createAccountButton.click();
+    });
   }
 
   async verifyRegistrationErrors(...errors: string[]): Promise<void> {
-    await expect(this.error.first()).toBeVisible({ timeout: 10000 });
-    const errorsFromPage = await this.error.all();
-    for (const error of errors) {
-      let isExist = false;
-      for (const item of errorsFromPage) {
-        const itemText = await item.textContent();
-        if (itemText && itemText.trim() === error) {
-          isExist = true;
-          break;
+    await test.step(`verifyRegistrationErrors`, async () => {
+      await expect(this.error.first()).toBeVisible({ timeout: 10000 });
+      const errorsFromPage = await this.error.all();
+      for (const error of errors) {
+        let isExist = false;
+        for (const item of errorsFromPage) {
+          const itemText = await item.textContent();
+          if (itemText && itemText.trim() === error) {
+            isExist = true;
+            break;
+          }
         }
+        expect(isExist, `${error} exist`).toBe(true);
       }
-      expect(isExist, `${error} exist`).toBe(true);
-    }
+    });
   }
 
   async checkBotWarning() {
-    await this.page.locator('button[data-qa-id="close-modal"]').click();
+    await test.step(`checkBotWarning`, async () => {
+      await this.page.locator('button[data-qa-id="close-modal"]').click();
+    });
   }
+
 }
