01109c139d500c489a5f046539084baeca96afe7
bakhmeteva <yuliia.b@taxer.ua>
2025-08-03 18:22:05 +0300

    test.steps

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
@@ -21,13 +20,13 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // }
   ],
 });
--- tests/zara.spec.ts
+++ tests/zara.spec.ts
@@ -1,46 +1,46 @@
 import { test } from '../src/fixtures/fixtures';
 
-test('Golden pass: Reject cookies, search product, manage cart, and register with validation', async ({
-                                                                                                        page,
-                                                                                                        basePage,
-                                                                                                        searchComponent,
-                                                                                                        searchResultPage,
-                                                                                                        cartPage,
-                                                                                                        registrationPage,
-                                                                                                      }) => {
-  const productName = `FLORAL PRINT MIDI DRESS`;
-  await test.step(`main page open`, async () => {
-    await page.goto('https://www.zara.com');
-    await basePage.clickRejectCookies();
-    await basePage.clickOnContinueButton();
+test('Golden pass: Reject cookies, search product, manage cart, and register with validation',
+  async ({
+           page,
+           basePage,
+           searchComponent,
+           searchResultPage,
+           cartPage,
+           registrationPage,
+         }) => {
+    const productName = `FLORAL PRINT MIDI DRESS`;
+    await test.step(`main page open`, async () => {
+      await page.goto('https://www.zara.com');
+      await basePage.clickRejectCookies();
+      await basePage.clickOnContinueButton();
+    });
+
+    await test.step(`add items to shopping bag`, async () => {
+      await searchComponent.searchProductByName(productName);
+      await searchResultPage.selectDifferentSizes(productName, `S`, `M`, `L`, `XS`);
+    });
+
+    await test.step(`shopping bag actions`, async () => {
+      await searchResultPage.clickOnShoppingBag();
+      await cartPage.verifyCartItemsCount(4);
+      await cartPage.deleteItemBySize(`M`, `S`);
+      await cartPage.verifyCartItemsCount(2);
+      await cartPage.clickToContinueButton();
+    });
+
+    await registrationPage.clickRegisterButton();
+
+    await test.step(`negative registration checks`, async () => {
+      await registrationPage.registerWithInvalidData();
+      await registrationPage.verifyRegistrationErrors(
+        `E-mailEnter a valid email address (example: email@email.com).`,
+        `PasswordEnter a secure password: At least 8 characters long, containing uppercase and lowercase letters and numbers.`,
+        `NameRequired field.`,
+        `NameRequired field.`,
+      );
+    });
+
+    await registrationPage.registerWithFakeData();
+    await registrationPage.checkBotWarning();
   });
-
-  await test.step(`add items to shopping bag`, async () => {
-    await searchComponent.searchProductByName(productName);
-    await searchResultPage.selectDifferentSizes(productName, `S`, `M`, `L`, `XS`);
-  });
-
-  await test.step(`shopping bag actions`, async () => {
-    await searchResultPage.clickOnShoppingBag();
-    await cartPage.verifyCartItemsCount(4);
-    await cartPage.deleteItemBySize(`M`, `S`);
-    await cartPage.verifyCartItemsCount(2);
-    await cartPage.clickToContinueButton();
-  });
-
-  await registrationPage.clickRegisterButton();
-
-  await test.step(`negative registration checks`, async () => {
-    await registrationPage.registerWithInvalidData();
-    await registrationPage.verifyRegistrationErrors(
-      `E-mailEnter a valid email address (example: email@email.com).`,
-      `PasswordEnter a secure password: At least 8 characters long, containing uppercase and lowercase letters and numbers.`,
-      `NameRequired field.`,
-      `NameRequired field.`,
-    );
-  });
-
-
-  await registrationPage.registerWithFakeData();
-  await registrationPage.checkBotWarning();
-});
